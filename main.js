630a20a5ce75a40c2796f230c54aaf5fb3615b48
Alan da Paixao Nonato Santos <alan.paixao@totvs.com.br>
2022-09-19 09:40:41 -0300

    Criação da tabela de tickets em HTML

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+//Módulos para controlar a vida útil do aplicativo e criar janela nativa do navegador
+const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-function createWindow () {
-  // Create the browser window.
+function createWindow() {
+  // Cria a janela do navegador.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
   })
 
-  // and load the index.html of the app.
+  //carrga o index.html do aplicativo.
   mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // Abra o DevTools/Inspecionar elemento.
+  //mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+// Este método será chamado quando o Electron terminar
+// inicialização e está pronto para criar janelas do navegador.
+// Algumas APIs só podem ser usadas após a ocorrência deste evento.
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
+    // No macOS é comum recriar uma janela no aplicativo quando o
+    // ícone dock é clicado e não há outras janelas abertas.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
+// Sai quando todas as janelas estão fechadas, exceto no macOS. Aí é comum
+// para que os aplicativos e sua barra de menu permaneçam ativos até que o usuário saia
+// explicitamente com Cmd + Q.
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Neste arquivo você pode incluir o resto do processo principal específico do seu aplicativo
+// código. Você também pode colocá-los em arquivos separados e exigi-los aqui.
